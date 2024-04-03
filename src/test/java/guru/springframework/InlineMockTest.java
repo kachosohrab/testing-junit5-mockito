d56c8c9c177e6e3d707e64a4756f1a67f0980dfd
Kacho Sohrab <sohrabkacho@gmail.com>
2024-04-03 23:06:10 +0530

    inline Mock

---
+++ src/test/java/guru/springframework/InlineMockTest.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class InlineMockTest {
+    
+    @Test
+    void testInLineMock() {
+        Map mockMap = mock(Map.class);
+        assertEquals(mockMap.size(), 0);
+    }
+}
